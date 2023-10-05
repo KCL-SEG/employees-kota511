21d40091715130f182ec8d8edb4f739cb86f98f9
Kota Amemiya <kotaamemiya@gmail.com>
2023-10-05 18:32:54 +0100

    Updated payment method.

--- employee.py
+++ employee.py
@@ -2,7 +2,7 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name, hours, hourly, salary, commNum, commCon,commBon):
+    def __init__(self, name, hours = 0, hourly = 0, salary = 0, commNum = 0, commCon = 0,commBon = 0):
         self.name = name
         self.hours = hours
         self.hourly = hourly
@@ -16,38 +16,39 @@ class Employee:
         return total_pay
 
     def __str__(self):
+        contract_about = ""
+        commission_about = ""
+        
+        if self.commCon:
+            commission_about += f" and receives a commission for {self.commNum} contract(s) at {self.commCon}/contract"
+        elif self.commBon:
+            commission_about += f" and receives a bonus commission of {self.commBon}"
+        
         if self.hourly:
-            if self.commBon:
-                return f"{self.name} works on a contract of {self.hours} hours at {self.hourly}/hour and receives a bonus commission of {self.commBon}. Their total pay is {self.get_pay()}."
-            elif self.commCon:
-                return f"{self.name} works on a contract of {self.hours} hours at {self.hourly}/hour and receives a commission for {self.commNum} contract(s) at {self.commCon}/contract. Their total pay is {self.get_pay()}."
-            else:
-                return f"{self.name} works on a contract of {self.hours} hours at {self.hourly}/hour. Their total pay is {self.get_pay()}."
+            contract_about = "contract"
+            sentence = f"{self.name} works on a {contract_about} of {self.hours} hours at {self.hourly}/hour{commission_about}. Their total pay is {self.get_pay()}."
         elif self.salary:
-            if self.commBon:
-                return f"{self.name} works on a monthly salary of {self.salary} and receives a bonus commission of {self.commBon}. Their total pay is {self.get_pay()}."
-            elif self.commCon:
-                return f"{self.name} works on a monthly salary of {self.salary} and receives a commission for {self.commNum} contract(s) at {self.commCon}/contract. Their total pay is {self.get_pay()}."
-            else:
-                return f"{self.name} works on a monthly salary of {self.salary}. Their total pay is {self.get_pay()}."
-
+            contract_about = "monthly salary"
+            sentence = f"{self.name} works on a {contract_about} of {self.salary}{commission_about}. Their total pay is {self.get_pay()}."
+    
+        return sentence
         
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie',0,0,4000,0,0,0)
+billie = Employee('Billie',salary = 4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie',100,25,0,0,0,0)
+charlie = Employee('Charlie',hours = 100,hourly = 25)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee',0,0,3000,4,200,0)
+renee = Employee('Renee',salary = 3000,commNum = 4,commCon = 200)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan',150,25,0,3,220,0)
+jan = Employee('Jan',hours = 150,hourly = 25, commNum = 3,commCon = 220)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie',0,0,2000,0,0,1500)
+robbie = Employee('Robbie',salary = 2000, commBon = 1500)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel',120,30,0,0,0,600)
+ariel = Employee('Ariel',hours = 120, hourly = 30, commBon = 600)
